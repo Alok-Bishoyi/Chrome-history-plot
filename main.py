e3878ea44a4d49e18ec10584e6b77f22707eeeb6
Zahid Wakeel <zhdwkl@gmail.com>
2018-03-03 12:47:40 +0530

    Fixed windows path error + shape mismatch error

--- main.py
+++ main.py
@@ -12,10 +12,14 @@ def parse(url):
 		domain = sublevel_split[0].replace("www.", "")
 		return domain
 	except IndexError:
-		print "Error reading URL"
+		print ("Error reading URL")	# print "" syntax no longer works for python v3.x
 
+# \\ used instead of \ due to :
+# Typical error on Windows because the default user directory is C:\user\<your_user>,
+# so when you want to use this path as an string parameter into a Python function,
+# you get a Unicode error, just because the \u is a Unicode escape. Any character not numeric after this produces an error.
 
-data_path = os.path.expanduser('~')+"\AppData\Local\Google\Chrome\User Data\Default" #path to user's history database (Chrome)
+data_path = os.path.expanduser('~')+"\\AppData\\Local\\Google\\Chrome\\User Data\\Default" #path to user's history database (Chrome)
 files = os.listdir(data_path)
 
 history_db = os.path.join(data_path, 'history')
@@ -40,13 +44,13 @@ for url, count in results:
 
 sites_count_sorted = OrderedDict(sorted(sites_count.items(), key=operator.itemgetter(1), reverse=True))
 
-index = [1,2,3, 4, 5, 6, 7, 8, 9, 10]
+index = [1,2,3, 4, 5, 6, 7, 8, 9]	# len(index) = 10(old) --> 9(new), else throwing shape mismatch error because len(index) != len(count).
 
-count = list(sites_count_sorted.values())[:10]
+count = list(sites_count_sorted.values())[:10]	# len(count) = 9
 
 LABELS = list(sites_count_sorted.items())[:10]
 
 
-plt.bar(index, count, align='center')
+plt.bar(index, count, align='center')	# length of index & count list must be same, otherwise will throw shape mismatch error.
 plt.xticks(index, LABELS)
 plt.show()
