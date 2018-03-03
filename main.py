e389dd0a43be6d1a63582f8c817a5391abae3660
Zahid Wakeel <zhdwkl@gmail.com>
2018-03-03 19:44:36 +0530

    Clear labels + shape mismatch fix improved

--- main.py
+++ main.py
@@ -44,13 +44,21 @@ for url, count in results:
 
 sites_count_sorted = OrderedDict(sorted(sites_count.items(), key=operator.itemgetter(1), reverse=True))
 
-index = [1,2,3, 4, 5, 6, 7, 8, 9]	# len(index) = 10(old) --> 9(new), else throwing shape mismatch error because len(index) != len(count).
+index = []	# Don't hardcode the indices. Append indices later depending on count length(max 10).
 
-count = list(sites_count_sorted.values())[:10]	# len(count) = 9
+count = list(sites_count_sorted.values())[:10]	# Whatever the no. of sites are, limit it to <= 10.
+
+# Making sure len(count) = len(index)
+l = len(count)
+for i in range (0, l):
+	index.append(i)
 
 LABELS = list(sites_count_sorted.items())[:10]
 
-
 plt.bar(index, count, align='center')	# length of index & count list must be same, otherwise will throw shape mismatch error.
-plt.xticks(index, LABELS)
+plt.xticks(index, LABELS, rotation = 14)	# default rotation = 0
 plt.show()
+
+# The program doesn't run while chrome is running (throws database lock error).
+# index & count length automated.
+# The labels appear congested, so rotation = 14 is applied.
